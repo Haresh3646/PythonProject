aa4da6e02d6678fa2c0532ac146978aa2d950962
Haresh3646 <haresh3646@gmail.com>
2025-06-27 21:12:57 +0530

    New file added

---
+++ .idea/p3.py
@@ -0,0 +1,13 @@
+from os import fdopen
+
+l = [10,20,30,40,50]
+print(l)
+l.append(30)
+print(l)
+l.insert(1,15)
+print (l)
+print(15 in l)
+print(l.count(30))
+print(l.index(30))
+print(l.index(30,4,7))
+print("")
---
+++ p5.py
@@ -0,0 +1,4 @@
+import datetime
+now = datetime.datetime.now()
+print("Current date time:")
+print(now.strftime("%d-%m-%y %H:%M:%S"))
